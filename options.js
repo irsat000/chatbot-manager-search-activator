d3af2dc6718515d2689a509e3888dd8651b14e19
irsat000 <irsat000@gmail.com>
2025-03-12 20:42:45 +0300

    already active and timed out states

--- options.js
+++ options.js
@@ -1,13 +1,25 @@
 const runtime = (typeof browser !== 'undefined') ? browser : chrome;
-const check = setInterval(() => {
-    if (getWindowParam("chatbot-manager-search-activated") == "true") {
+let check = setInterval(() => {
+    const isActive = getWindowParam("chatbot-manager-search-activated")
+    if (isActive == "true") {
         clearInterval(check);
+        check = null;
         document.querySelector('.success').classList.add('active');
-        setTimeout(() => {
-            runtime.management.uninstallSelf();
-        }, 4000);
+        setTimeout(() => runtime.management.uninstallSelf(), 4000);
+    }
+    else if (isActive == "already-active") {
+        clearInterval(check);
+        check = null;
+        document.querySelector('.already-active').classList.add('active');
+        setTimeout(() => runtime.management.uninstallSelf(), 4000);
     }
 }, 50);
+setTimeout(() => {
+    if (!check) return;
+    clearInterval(check);
+    document.querySelector('.timed-out').classList.add('active');
+    setTimeout(() => runtime.management.uninstallSelf(), 10_000);
+}, 10_000);
 function getWindowParam(param) {
     const params = new URLSearchParams(window.location.search);
     return params.get(param);
