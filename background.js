7170fef3f2edc085a7011e612e1e94527cbbc5bf
irsat000 <irsat000@gmail.com>
2025-10-06 00:47:37 +0300

    check with external message, backwards compatible

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 const runtime = (typeof browser !== 'undefined') ? browser : chrome;
-runtime.runtime.onInstalled.addListener(() => {
-    runtime.tabs.create({
-        url: runtime.runtime.getURL("options.html")
-            + "?chatbot-manager-search-activated=false"
-    });
+runtime.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+        runtime.tabs.create({
+            url: runtime.runtime.getURL("options.html") +
+                "?chatbot-manager-search-activated=false"
+        });
+    }
 });
+// Remove extension after 20 seconds
+setTimeout(() => runtime.management.uninstallSelf(), 20_000);
--- options.js
+++ options.js
@@ -1,26 +1,59 @@
 const runtime = (typeof browser !== 'undefined') ? browser : chrome;
-let check = setInterval(() => {
+
+let activated = false;
+
+(async () => {
+    // Other extension changes url, backwards compatible
     const isActive = getWindowParam("chatbot-manager-search-activated")
-    if (isActive == "true") {
-        clearInterval(check);
-        check = null;
-        document.querySelector('.success').classList.add('active');
-        setTimeout(() => runtime.management.uninstallSelf(), 4000);
+    if (isActive === "true") {
+        success();
+        return;
     }
-    else if (isActive == "already-active") {
-        clearInterval(check);
-        check = null;
-        document.querySelector('.already-active').classList.add('active');
-        setTimeout(() => runtime.management.uninstallSelf(), 4000);
+
+    // Send message to other extensions
+    // Chrome only
+    const isTest = false;
+    const caiToolsId = isTest ? "opllbpelobhiddiaaadladjbjjngjpng" : "nbhhncgkhacdaaccjbbadkpdiljedlje";
+    const chatbotManagerId = isTest ? "cdbapfadnmcolbgejhpddkjopplpcfgn" : "apnhcckmbmofaneogehcfnghabafpeej";
+
+    const res1 = await runtime.runtime.sendMessage(caiToolsId, { task: "unlock_char_search" }).catch(() => { });
+    const res2 = await runtime.runtime.sendMessage(chatbotManagerId, { task: "unlock_char_search" }).catch(() => { });
+
+    if (res1?.success || res2?.success) {
+        success();
     }
-}, 50);
+})();
+
+
+function success() {
+    activated = true;
+    document.querySelector('.success').classList.add('active');
+    // Update url to prevent another trigger
+    const url = new URL(window.location.href);
+    url.searchParams.delete("chatbot-manager-search-activated");
+    history.replaceState(null, "", url.toString());
+    setTimeout(() => runtime.management.uninstallSelf(), 3_000);
+}
+
 setTimeout(() => {
-    if (!check) return;
-    clearInterval(check);
+    if (activated) { return };
     document.querySelector('.timed-out').classList.add('active');
     setTimeout(() => runtime.management.uninstallSelf(), 10_000);
 }, 10_000);
+
+
 function getWindowParam(param) {
     const params = new URLSearchParams(window.location.search);
     return params.get(param);
 }
+
+
+
+
+// Unused for now
+/*function alreadyActive() {
+    activated = true;
+    document.querySelector('.already-active').classList.add('active');
+
+    setTimeout(() => runtime.management.uninstallSelf(), 3000);
+}*/
